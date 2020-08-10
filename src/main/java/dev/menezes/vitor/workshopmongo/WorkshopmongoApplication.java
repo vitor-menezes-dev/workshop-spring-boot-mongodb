988ded69b78e9955aec0c73b63da5ca0aacb73dc
Vitor Andrade Menezes de Sousa <vitor.menezes.dev@gmail.com>
2020-08-10 14:01:32 -0300

    Projeto Criado

---
+++ src/main/java/dev/menezes/vitor/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package dev.menezes.vitor.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/menezes/vitor/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.menezes.vitor.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
